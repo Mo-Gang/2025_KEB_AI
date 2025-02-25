532e21eef45bb280cbcb9c1f67a04c7796ef1ea8
MoGang <mkm9542@inha.edu>
2025-02-25 09:27:18 +0900

    v11.0 assignment

--- day11.py
+++ day11.py
@@ -1,63 +1,55 @@
-# import numpy as np
-#
-# v = np.array([1, 3.9, -9, 2])
-# print(v, v.ndim)
+# day11 assignment) v0.6의 최근접이웃모델과 같이 동작하는 커스텀 클래스를 설계하시오.
 
-
-
-
-
-# import numpy as np
-# import pandas as pd
-#
-# df = pd.DataFrame(
-#     {"a" : [4, 5, 6],
-#           "b" : [7, 8, 9],
-#           "c" : [10, 11, 12]}, index = [1, 2, 3]
-# )
-# print(df)
-
-
-
-
-
-# import numpy as np
-# import pandas as pd
-#
-# df = pd.DataFrame(
-#     [[4, 7, 10],
-#      [5, 8, 11],
-#      [6, 9, 12]], index=[1, 2, 3], columns=['a', 'b', 'c']
-# )
-# print(df)
-
-
-
-
-
-
-# #from statistics import LinearRegression
-# from sklearn.linear_model import LinearRegression
-# # from sklearn.neighbors import KNeighborsRegressor
+# v0.6 최근접이웃모델)
+# # from sklearn.linear_model import LinearRegression
+# # import tglearn as tg
+# # from tglearn import LinearRegression
+# # from tglearn import KNeighborsRegressor
+# from sklearn.neighbors import KNeighborsRegressor
 # import pandas as pd
 # import matplotlib.pyplot as plt
 #
 # ls = pd.read_csv("https://github.com/ageron/data/raw/main/lifesat/lifesat.csv")
-# #print(type(ls))
-# #print(ls)
 # X = ls[["GDP per capita (USD)"]].values
 # y = ls[["Life satisfaction"]].values
-# #print(X)
 #
-# # ls.plot(kind='scatter', grid=True, x="GDP per capita (USD)", y="Life satisfaction")
-# # plt.axis([23500, 62500, 4, 9])
-# # plt.show()
+# ls.plot(kind='scatter', grid=True, x="GDP per capita (USD)", y="Life satisfaction")
+# plt.axis([23500, 62500, 4, 9])
+# plt.show()
 #
-# model = LinearRegression()
-# # model = KNeighborsRegressor(n_neighbors=3)
-# model.fit(X, y)  # 머신 러닝
+# #model = LinearRegression()
+# model = KNeighborsRegressor()
+# model.fit(X, y)
 #
 # X_new = [[31721.3]]  # ROK 2020
 # print(model.predict(X_new))
 # # LinearRegression 5.90
 # # KNeighborsRegressor 5.70
+
+
+
+# day11 assignment 커스텀 클래스)
+# from sklearn.linear_model import LinearRegression
+# import tglearn as tg
+# from tglearn import LinearRegression
+from tglearn import KNeighborsRegressor
+# from sklearn.neighbors import KNeighborsRegressor
+import pandas as pd
+import matplotlib.pyplot as plt
+
+ls = pd.read_csv("https://github.com/ageron/data/raw/main/lifesat/lifesat.csv")
+X = ls[["GDP per capita (USD)"]].values
+y = ls[["Life satisfaction"]].values
+
+ls.plot(kind='scatter', grid=True, x="GDP per capita (USD)", y="Life satisfaction")
+plt.axis([23500, 62500, 4, 9])
+plt.show()
+
+#model = LinearRegression()
+model = KNeighborsRegressor(3)
+model.fit(X, y)
+
+X_new = [[31721.3]]  # ROK 2020
+print(model.predict(X_new))
+# LinearRegression 5.90
+# KNeighborsRegressor 5.70
